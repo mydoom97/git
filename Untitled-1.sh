30f025408fd5ce40ff7d951a6f60d26daba985d4
mydoom <3125957473@qq.com>
2023-09-07 21:19:32 +0800

    master

---
+++ Untitled-1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+wget -C https://storage.googleapis.com/golang/go1.13.3.linux-amd64.tar.gz
+tar -zxvf go1.13.3.linux-amd64.tar.gz  -C /usr/local
+echo "export PATH=$PATH:/usr/local/go/bin" > /etc/profile.d/go.sh
+source /etc/profile.d/go.sh 
+go version
